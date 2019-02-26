8877903c5f50eb935d94e3bedf3dc1e5124e5b19
dgolverdingen12 <dgolverdingen@competa.com>
2019-02-26 08:48:08 +0100

    Changed the gulpfile

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -2,19 +2,15 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-// var nano = require('gulp-cssnano');
 
 gulp.task('default', defaultTask);
 
 function defaultTask(done) {
-  // place code for your default task here
   done();
 }
 
 gulp.task('sass', function() {
-  return gulp.src('scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass())
-    // .pipe(nano())
+  return gulp.src('scss/**/*.scss') 
     .pipe(gulp.dest('./'))
 })
 
